7d35eed9d53f02f85f8ae2eaad2a4af6e12c305a
Viktar Rusakovich <mrisviz.rus@gmail.com>
2024-03-04 15:07:13 +0300

    feat: add init.sh fix: docker-compose

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd micro-exception-starter
+gradle build publish
+
+cd ../logger-aspect-starter
+gradle build publish
+
+cd ../micro-news
+gradle build -x test
+
+cd ../micro-comments
+gradle build -x test
+
+cd ../micro-search
+gradle build -x test
+
+cd ../micro-gateway
+gradle build -x test
+
+cd ..
+docker-compose up --build
